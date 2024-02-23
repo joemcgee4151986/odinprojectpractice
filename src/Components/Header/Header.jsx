83c2de7b3f53f3b9ab9e1c191cb20b792fe66470
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-02-23 15:00:01 +0900

    added a button to record clicks. I did this to practice useState

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import {Headerfont, Middlefont} from '../styled-component-files/Header-style';
-import React from 'react';
+import React, { useState } from 'react';
+
+
+
 const Header = () => {
+    const [count, setCount] = useState(0);
+    const handleClick =()=> {
+        setCount (count + 1);
+    }
     return(
         <Middlefont>
         <Headerfont>Best Pizza in town! Yummy!</Headerfont>
+        <button onClick = {handleClick}> I was pressed {count} times!</button>
         </Middlefont>
+     
     )
 }
+
 export {Header};
